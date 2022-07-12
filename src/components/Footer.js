be715f6a78708167b6153f34561a4008570525e0
Prashant Gangwar <prashantgangwar45@gmail.com>
2022-07-12 13:26:22 +0530

    keeper-heroku-hosting

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+function Footer(){
+    return (
+        <footer><p> Made by Prashant Gangwar</p> </footer>
+    );
+}
+
+export default Footer
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//both dependencies get automatically installed when you create a react app
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById("root"));
